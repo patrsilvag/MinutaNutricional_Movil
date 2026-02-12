fdf363874d7d6820d5138251fa9f56bea5f12178
patrsilvag <patr.silvag@duocuc.cl>
2026-02-12 19:57:05 -0300

    Limpieza: eliminación total de XML y dependencias clásicas. Proyecto 100% Jetpack Compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,8 +66,5 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.fragment:fragment-ktx:1.6.2")
-
 
 }
